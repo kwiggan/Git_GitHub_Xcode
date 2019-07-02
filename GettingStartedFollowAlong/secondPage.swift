a2489fde800be5ef8dc46a3312af380cc1076a24
Keneisha Wiggan <keneishawiggan8@gmail.com>
2019-07-02 05:38:22 -0700

    practise: Adding background image

---
+++ GettingStartedFollowAlong/secondPage.swift
@@ -0,0 +1,13 @@
+//
+//  secondPage.swift
+//  GettingStartedFollowAlong
+//
+//  Created by Keneisha Wiggan on 7/2/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import UIKit
+
+class secondPage: NSObject {
+
+}
